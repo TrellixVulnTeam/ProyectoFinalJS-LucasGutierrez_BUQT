6cbc65ffdd7a0aeb9fc213f3e9f3f1437840bf22
LucasGut05 <70763377+LucasGut05@users.noreply.github.com>
2021-10-27 21:56:46 -0300

    mejora etiquetas de productos

--- scripts/productos.js
+++ scripts/productos.js
@@ -9,23 +9,53 @@ if (localStorage.getItem("lista") == null) {
 } else {
   listaObjetos = JSON.parse(localStorage.getItem("lista"));
   listaObjetos.forEach((element) => {
+    const listaPA = document.createElement("div");
+    listaPA.setAttribute("class", "col-4");
+    
+    listaPA.setAttribute("style", "width: 18rem;");
+
     const listaP = document.createElement("div");
-    const listaPN = document.createElement("p");
+    listaP.setAttribute("class", "card-body");
+
+    const listaPN = document.createElement("h5");
+    listaPN.setAttribute("class", "card-title");
     listaPN.textContent = element.nombre;
     listaP.appendChild(listaPN);
-    const listaPC = document.createElement("p");
+
+    const listaPC = document.createElement("h6");
+    listaPC.setAttribute("class", "card-subtitle");
+    listaPC.setAttribute("class", "mb-2");
+    listaPC.setAttribute("class", "text-muted");
     listaPC.textContent = element.categoria;
     listaP.appendChild(listaPC);
-    const listaPG = document.createElement("p");
+
+    const listaPG = document.createElement("h6");
+    listaPG.setAttribute("class", "card-subtitle");
+    listaPG.setAttribute("class", "mb-2");
+    listaPG.setAttribute("class", "text-muted");
     listaPG.textContent = element.genero;
     listaP.appendChild(listaPG);
-    const listaPP = document.createElement("p");
+
+    const listaPP = document.createElement("h6");
+    listaPP.setAttribute("class", "card-subtitle");
+    listaPP.setAttribute("class", "mb-2");
+    listaPP.setAttribute("class", "text-muted");
     listaPP.textContent = element.precio;
     listaP.appendChild(listaPP);
-    const listaPS = document.createElement("p");
+
+    const listaPS = document.createElement("h6");
+    listaPS.setAttribute("class", "card-subtitle");
+    listaPS.setAttribute("class", "mb-2");
+    listaPS.setAttribute("class", "text-muted");
     listaPS.textContent = element.stock;
     listaP.appendChild(listaPS);
 
-    document.getElementById("crearProductos").appendChild(listaP);
+    const listaBoton = document.createElement("button");
+    listaBoton.textContent = "COMPRAR";
+    listaP.appendChild(listaBoton);
+
+    listaPA.appendChild(listaP);
+
+    document.getElementById("crearProductos").appendChild(listaPA);
   });
 }
