0fb3c5ca1008c7dfb2d8a20c25d82e9b96439277
LucasGut05 <70763377+LucasGut05@users.noreply.github.com>
2021-10-29 19:31:26 -0300

    avavnce 29/10

--- scripts/productos.js
+++ scripts/productos.js
@@ -1,17 +1,11 @@
 let listaObjetos;
 
-if (localStorage.getItem("lista") == null) {
-  const listaP = document.createElement("h2");
-  listaP.textContent = "No hay productos disponibles";
-  document.getElementById("crearProductos").appendChild(listaP);
+//Función para generar los productos en el HTML
 
-  listaObjetos = [];
-} else {
-  listaObjetos = JSON.parse(localStorage.getItem("lista"));
-  listaObjetos.forEach((element) => {
+let funcionCrearCard = (lista) => {
+  lista.forEach((element) => {
     const listaPA = document.createElement("div");
     listaPA.setAttribute("class", "col-4");
-    
     listaPA.setAttribute("style", "width: 18rem;");
 
     const listaP = document.createElement("div");
@@ -51,6 +45,7 @@ if (localStorage.getItem("lista") == null) {
     listaP.appendChild(listaPS);
 
     const listaBoton = document.createElement("button");
+    listaBoton.setAttribute("id", "boton");
     listaBoton.textContent = "COMPRAR";
     listaP.appendChild(listaBoton);
 
@@ -58,4 +53,72 @@ if (localStorage.getItem("lista") == null) {
 
     document.getElementById("crearProductos").appendChild(listaPA);
   });
-}
+};
+
+//Fin función para generar los productos en el HTML
+
+//Funcion para imprimir los productos cargados en el backoffice
+
+let funcionImprimirProducto = () => {
+  if (localStorage.getItem("lista") == null) {
+    const listaP = document.createElement("h2");
+    listaP.textContent = "No hay productos disponibles";
+    document.getElementById("crearProductos").appendChild(listaP);
+    listaObjetos = [];
+  } else {
+    listaObjetos = JSON.parse(localStorage.getItem("lista"));
+    funcionCrearCard(listaObjetos);
+  }
+};
+
+//Fin funcion para imprimir los productos cargados en el backoffice
+
+//Funcion para filtrar productos
+
+let listaFiltrada;
+
+let dato;
+
+let funcionFiltrarPorNombre = (lista) => {
+  dato = document.getElementsByTagName("select")[0].value;
+  if (dato === "ninguno") {
+    funcionImprimirProducto();
+  } else {
+    listaFiltrada = lista.filter((element) => element.nombre === dato);
+    if (listaFiltrada.length != 0) {
+      funcionCrearCard(listaFiltrada);
+    } else {
+      const listaP = document.createElement("h2");
+      listaP.textContent = "No hay productos disponibles";
+      document.getElementById("crearProductos").appendChild(listaP);
+    }
+  }
+};
+
+//Fin funcion para filtrar productos
+
+//funcion comprar, validamos si carrito esta vacio: inicializamos en 0; si hay archivos en el localstorage "carrito" lo traemos
+/*const comprar = (dato) => {
+  let carrito;
+
+  if (localStorage.getItem("carrito") == null) {
+    carrito = [];
+  } else {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+  }
+
+  carrito.push(dato);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+*/
+
+//Llamada a funciones
+
+funcionImprimirProducto(listaObjetos);
+
+const botonFiltrar = document.getElementsByTagName("button")[2];
+
+botonFiltrar.addEventListener("click", () => {
+  funcionFiltrarPorNombre();
+});
